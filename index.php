97ef5c016906a1e84c57aa680dcae09c58674027
SiberianEagle <45014857+SiberianEagle@users.noreply.github.com>
2018-12-10 17:23:20 +0600

    поправил

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php
 require_once 'function.php';
-require_once 'price_function.php';
 require_once 'arrays.php';
-
+require_once 'constants.php';
 $is_auth = rand(0, 1);
-$user_name = 'Сергей Орлов';
 $user_avatar = 'img/user.jpg';
 
-$page_content = include_template('index.php', ['categories' => $categories, 'items' => $items]);
+$page_content = include_template('index.php', 
+	[
+	 'categories' => $categories,
+	 'items' => $items
+	]);
 
-$layout_content = include_template('layout.php', ['content' => $page_content, 'categories' => $categories, 'title' => 'Главная страница', 'is_auth' => $is_auth]);
+$layout_content = include_template('layout.php', 
+	[
+	'content' => $page_content,
+	'categories' => $categories,
+	'title' => 'Главная страница',
+	'is_auth' => $is_auth
+]);
 
 print($layout_content);
 
